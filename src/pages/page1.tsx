225aa1e6a300e5f52f50ace59f3be7e9ce79367f
微凉 <i@nn.ci>
2022-01-30 11:37:45 +0800

    Update page1.tsx

--- src/pages/page1.tsx
+++ src/pages/page1.tsx
@@ -1,9 +1,18 @@
-import { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 
 const Page1: Component = () => {
+  const [get, set] = createSignal(0);
   return (
     <div>
       <p class="text-4xl text-green-700 text-center py-20">Page1</p>
+      <button
+        onClick={() => {
+          set(get() + 1);
+        }}
+        class="btn btn-info"
+      >
+        {get()}
+      </button>
     </div>
   );
 };
